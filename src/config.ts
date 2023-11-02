9252b536a4481f355014b422df11b6073e2c0237
shubham dhumal <94701914+sd025@users.noreply.github.com>
2023-11-02 20:38:11 +0530

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,34 @@
+import * as vscode from 'vscode';
+
+const CSConfig = {
+    SEARCH_PATTERN: /(\/\/|#|--|<!--)\s?find\s?(.+)\s?(\.|-->)/
+};
+
+export function getSearchURL(site: string, keyword: string) {
+    return `https://www.google.com/search?q=site%3A${site}+${keyword.replace(/\s/g, "+")}`;
+}
+
+type IConfig = {
+    settings: {
+        sites: { [name: string]: boolean },
+        maxResults: number
+    }
+}
+
+export function getConfig() {
+    const config = vscode.workspace.getConfiguration("autocomplete");
+
+    const sites = {
+        "stackoverflow.com": config.settings.sites.stackoverflow,
+        "gist.github.com": config.settings.sites.githubGist
+    };
+
+    return {
+        settings: {
+            sites,
+            maxResults: config.settings.maxResults
+        }
+    } as IConfig;
+}
+
+export default CSConfig;
